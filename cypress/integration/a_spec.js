82b9e4a759c2e0b3a3501a446d2c68270820746b
estrada9166 <estrada9166@hotmail.com>
2021-06-16 14:57:12 -0500

    Update action

--- cypress/integration/a_spec.js
+++ cypress/integration/a_spec.js
@@ -1,5 +1,5 @@
 describe("Flaky test", function () {
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= 1000; i++) {
     it(`should pass ${i * 1}% of the time`, function () {
       expect(1).to.be.eq(1);
     });
