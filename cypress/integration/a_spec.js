f8d1912cd0afd9bb07f9036accaafe5f56460f8c
estrada9166 <estrada9166@hotmail.com>
2021-06-16 14:25:21 -0500

    Update action

--- cypress/integration/a_spec.js
+++ cypress/integration/a_spec.js
@@ -1,15 +1,13 @@
 describe("Flaky test", function () {
   for (let i = 1; i <= 100; i++) {
     it(`should pass ${i * 1}% of the time`, function () {
-      const subject = 100 * Math.random();
-      expect(subject).to.be.lt(i / 1);
+      expect(1).to.be.lt(1);
     });
 
     describe("with a really long test name", function () {
       describe("like no, really really long", function () {
         it(`should pass ${i * 1}% of the time`, function () {
-          const subject = 100 * Math.random();
-          expect(subject).to.be.lt(i / 1);
+          expect(1).to.be.lt(1);
         });
       });
     });
